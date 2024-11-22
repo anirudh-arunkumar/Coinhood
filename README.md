5e90e0099fbc9173202ee92e633cdcf829990b00
Anirudh Arunkumar <aarunkumar8@gatech.edu>
2024-11-21 20:23:46 -0500

    refactor

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Coinhood
-A trading tool using Robinhood's Crypto API to execute trades on popular crypto currencies and compare prices with other brokerages.
+A trading tool using Robinhood's Crypto API to execute trades on popular crypto currencies
--- generate_secrets.py
+++ generate_secrets.py
@@ -7,8 +7,5 @@ public_key = private_key.verify_key
 private_key_base64 = base64.b64encode(private_key.encode()).decode()
 public_key_base64 = base64.b64encode(public_key.encode()).decode()
 
-print("Private Key (Base64):")
-print(private_key_base64)
-
-print("Public Key (Base64):")
-print(public_key_base64)
+print("Private Key (Base64):", private_key_base64)
+print("Public Key (Base64):", public_key_base64)
